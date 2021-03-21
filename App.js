ad2870b223ef6e5bff69325e22872f7a4b57cb9e
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-21 21:55:19 +0700

    Refactor code and add update and delete capability

--- App.js
+++ App.js
@@ -1,33 +1,87 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { AddTodo } from './src/AddTodo';
-import { NavBar } from './src/NavBar';
-import { Todo } from './src/Todo';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { NavBar } from './src/components/NavBar';
+import { MainScreen } from './src/screens/MainScreen';
+import { TodoScreen } from './src/screens/TodoScreen';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todoId, setTodoId] = useState(null);
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'Когда же это кончится' },
+    { id: '2', title: 'Быстрей бы весна' },
+    { id: '3', title: 'а потом сессия...' },
+  ]);
 
   const addTodo = (title) => {
     setTodos((prev) => [
       ...prev,
       {
         id: Date.now().toString(),
-        title: title,
+        title,
       },
     ]);
   };
 
+  const removeTodo = (id) => {
+    const todo = todos.find((t) => t.id === id);
+    Alert.alert(
+      'Удаление элемента',
+      `Вы уверены, что хотите удалить ${todo.title}`,
+      [
+        {
+          text: 'Отмена',
+          style: 'cancel',
+        },
+        {
+          text: 'Удалить',
+          onPress: () => {
+            setTodoId(null);
+            setTodos((prev) => prev.filter((todo) => todo.id !== id));
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const updateTodo = (id, title) => {
+    setTodos((old) =>
+      old.map((todo) => {
+        if (todo.id === id) {
+          todo.title = title;
+        }
+        return todo;
+      })
+    );
+  };
+
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodoId}
+    />
+  );
+
+  if (todoId) {
+    const selectedTodo = todos.find((todo) => todo.id === todoId);
+    content = (
+      <TodoScreen
+        goBack={() => {
+          setTodoId(null);
+        }}
+        todo={selectedTodo}
+        onRemove={removeTodo}
+        onSave={updateTodo}
+      />
+    );
+  }
+
   return (
     <View>
       <NavBar title="Todo App" />
-      <View style={styles.container}>
-        <AddTodo onSubmit={addTodo} />
-        <View>
-          {todos.map((todo) => (
-            <Todo key={todo.id} todo={todo} />
-          ))}
-        </View>
-      </View>
+      <View style={StyleSheet.container}>{content}</View>
     </View>
   );
 }
